bfa327acef32396c0db157959dea6d30213a4e88
65985 <65985@LAPTOP-PTUQ55JC>
2021-07-16 20:56:47 +0800

    HelloWorld modified with new line to print.

--- C206_P09/src/HelloWorld.java
+++ C206_P09/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("My first java program.");
+		System.out.println("My first java program version 2.");
 	}
 
 }
